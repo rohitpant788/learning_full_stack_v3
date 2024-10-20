f31cd17a30217cb840f595d74bd3a028d50d6fe7
rohit <rohit.pant.788@gmail.com>
2024-10-20 05:54:15 +0530

    UseEffect

--- week-9-react-practise/src/App.jsx
+++ week-9-react-practise/src/App.jsx
@@ -1,30 +1,32 @@
-import { useState } from "react"
-
+import { useEffect, useState } from "react"
 
 function App() {
-
   return <>
-    <Counter/>
+    <Counter />
   </>
 }
 
-function Counter(){
-
-
+function Counter() {
   //React doesnot rerenders itself if i create count as normal variables , 
   //rather it only rerenders if this is state variable 
   const [count, setCount] = useState(0);
 
-  function increaseCount(){
-    setCount(count +1)
-  }
+  console.log("counter");
+  useEffect(function () {
+    setInterval(
+      function () {
+        setCount(function (count) {
+          return count + 1;
+        })
+      }, 
+      1000)
+      console.log("mounted");
+  }, []);
 
   return <>
-    <b>{count}</b><br/>
-    <button onClick={increaseCount}>Increase Count</button>
+    <b>{count}</b><br />
   </>
 
 }
-
 
 export default App
