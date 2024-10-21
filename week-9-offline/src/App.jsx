9067432543c3b8d63af36f38d66be30739544d71
rohit <rohit.pant.788@gmail.com>
2024-10-21 22:24:50 +0530

    Timer with Cleanup Functionality

--- week-9-offline/src/App.jsx
+++ week-9-offline/src/App.jsx
@@ -4,44 +4,38 @@ import { useEffect } from "react"
 
 function App() {
 
-  const [currenTab, setCurrenTab] = useState(1)
-  const [tabData, setTabData] = useState({})
-  const [loading, setLoading] = useState(true)
+  const [showTimer, setShowTimer] = useState(true)
+
+  useEffect(function (){
+    setInterval(function (){
+      setShowTimer(function(){
+        return !showTimer;
+      })
+    },5000);
+  }), [];
+
+return <div>
+  {showTimer && <Timer/>}
+</div>
+}
+
+function Timer (){
+  const [seconds, setSeconds] = useState(0)
 
   useEffect(function(){
-    console.log("fetching data for tab #" +currenTab)
-    setLoading(true)
-    fetch("https://jsonplaceholder.typicode.com/todos/"+currenTab)
-    .then( async res => {
-      const json = await res.json();
-      setTabData(json);
-      setLoading(false);
-    })
 
+    let clock = setInterval(function(){
+      return setSeconds(function(currentSeconds){
+        return currentSeconds + 1;
+      })
+    },1000);
 
-  },[currenTab])
+    return function(){
+      clearInterval(clock);
+    }
+  }),[];
 
-
-
-  return (
-    <div>
-      <button onClick={function () {
-        setCurrenTab(1)
-      }} style={{ color: currenTab == 1 ? "red" : "black" }}>Todo #1</button>
-      <button onClick={function () {
-        setCurrenTab(2)
-      }} style={{ color: currenTab == 2 ? "red" : "black" }}>Todo #2</button>
-      <button onClick={function () {
-        setCurrenTab(3)
-      }} style={{ color: currenTab == 3 ? "red" : "black" }}>Todo #3</button>
-      <button onClick={function () {
-        setCurrenTab(4)
-      }} style={{ color: currenTab == 4 ? "red" : "black" }}>Todo #4</button>
-
-      <br/>
-      {loading ? "Loading........." : tabData.title}
-    </div>
-  )
+  return <div>{seconds } seconds Lapsed...</div>
 }
 
 export default App
