30abe70a6eb23e15bfcaa97b095c7a7b162d094d
rohit <rohit.pant.788@gmail.com>
2024-10-31 17:08:49 +0530

    Understanding Layout and Outlet Component in React Router DOM

--- week-10-routing/src/App.jsx
+++ week-10-routing/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter, Link, Route, Routes, useNavigate } from 'react-router-dom'
+import { BrowserRouter, Link, Outlet, Route, Routes, useNavigate } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,18 +7,37 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Link to="/">Allen</Link> | 
-        <Link to="/neet/online-coaching-class-11">Class11</Link> | 
-        <Link to="/neet/online-coaching-class-12">Class12</Link>
+
         <Routes>
-          <Route path="/" element={<LandingPage />}></Route>
-          <Route path="/neet/online-coaching-class-11" element={<Class11Program />}></Route>
-          <Route path="/neet/online-coaching-class-12" element={<Class12Program />}></Route>
-          <Route path="/*" element={<NoPage />}></Route>
+          <Route path="/" element={<Layout/>}>
+            <Route path="/" element={<LandingPage />}></Route>
+            <Route path="/neet/online-coaching-class-11" element={<Class11Program />}></Route>
+            <Route path="/neet/online-coaching-class-12" element={<Class12Program />}></Route>
+            <Route path="/*" element={<NoPage />}></Route>
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
   )
+}
+
+function Layout() {
+  return <div style={{background:"green", height: "100vh"}}> 
+    <Header/>
+    <div style={{background:"red", height: "90vh"}}>
+      <Outlet/>
+    </div>
+    Footer!
+  </div>
+}
+
+
+function Header() {
+  return <div>
+    <Link to="/">Allen</Link> | 
+        <Link to="/neet/online-coaching-class-11">Class11</Link> | 
+        <Link to="/neet/online-coaching-class-12">Class12</Link>
+  </div>
 }
 
 function NoPage(){
