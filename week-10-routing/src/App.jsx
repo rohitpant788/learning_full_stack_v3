39f0f33821cbf267d8e66072d54723b967fc5180
rohit <rohit.pant.788@gmail.com>
2024-10-31 06:08:58 +0530

    Navigating with the Link Component in React Router DOM

--- week-10-routing/src/App.jsx
+++ week-10-routing/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,10 +7,13 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        <Link to="/">Allen</Link> | 
+        <Link to="/neet/online-coaching-class-11">Class11</Link> | 
+        <Link to="/neet/online-coaching-class-12">Class12</Link>
         <Routes>
-          <Route path="/" element={<LandingPage/>}></Route>
-          <Route path="/neet/online-coaching-class-11" element={<Class11Program/>}></Route>
-          <Route path="/neet/online-coaching-class-12" element={<Class12Program/>}></Route>
+          <Route path="/" element={<LandingPage />}></Route>
+          <Route path="/neet/online-coaching-class-11" element={<Class11Program />}></Route>
+          <Route path="/neet/online-coaching-class-12" element={<Class12Program />}></Route>
         </Routes>
       </BrowserRouter>
     </>
@@ -18,15 +21,15 @@ function App() {
 }
 
 
-function LandingPage(){
+function LandingPage() {
   return <div>Welcome to Allen... !</div>
 }
 
-function Class11Program(){
+function Class11Program() {
   return <div> This is for Class 11 Program...!</div>
 }
 
-function Class12Program(){
+function Class12Program() {
   return <div> This is for Class 12 Program...!</div>
 }
 
